de46f0f711348ad40f11512f102be455ea9e06bd
GautamKr2 <gautamkumar11008@gmail.com>
2026-09-10 18:00:48 +0530

    Designed Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <NavBar />
-      <h1> Kali Puja Frontend </h1>
+      <div className="relative h-3/4 bg-red-500">
+        <NavBar />
+      </div>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="/about" element={<h1>About Page</h1>}/>
+        <Route path="/members" element={<h1>Members Page</h1>}/>
+        <Route path="/gallery" element={<h1>Gallery Page</h1>}/>
+        <Route path="/contact" element={<h1>Contact Page</h1>}/>
+      </Routes>
+      <h1>Home Page</h1>
+      <h1>About Page</h1>
+      <h1>Members Page</h1>
+      <h1>Gallery Page</h1>
+      <h1>Contact Page</h1>
     </>
   )
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,40 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 export default function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
-            <h2 className="text-3xl font-bold"> Navbar Creation Page </h2>
+            <nav className="absolute left-1/2 -translate-x-1/2 mt-5 md:mt-8 w-[96%] h-10 md:h-16 flex justify-between items-center bg-red-700 border-[1px] md:border-[1.6px] border-solid border-red-700 rounded-lg text-center">
+            
+                <div className="ml-3 md:ml-5">
+                    <img src="/src/assets/logo.png" alt="Logo" className="size-14 md:size-24 rounded-full" />
+                </div>
+
+                <div className="hidden md:flex gap-8 mr-6">
+                    <Link to="/" className="nav-link"> Home </Link>
+                    <Link to="/about" className="nav-link"> About </Link>
+                    <Link to="/members" className="nav-link"> Members </Link>
+                    <Link to="/gallery" className="nav-link"> Gallery </Link>
+                    <Link to="/contact" className="nav-link"> Contact </Link>
+                </div>
+
+                <button className="mr-5 md:hidden text-2xl" onClick={() => setMenuOpen(!menuOpen)}> ☰ </button>
+
+                {/* Mobile Menu */}
+                {
+                    menuOpen && (
+                        <div className="absolute top-full right-0 w-1/2 flex flex-col bg-red-600 rounded-lg py-4 gap-4">
+                            <Link to="/" className="nav-link">Home</Link>
+                            <Link to="/about" className="nav-link">About</Link>
+                            <Link to="/members" className="nav-link">Members</Link>
+                            <Link to="/gallery" className="nav-link">Gallery</Link>
+                            <Link to="/contact" className="nav-link">Contact</Link>
+                        </div>
+                    )
+                }
+            </nav>
         </>
     )
 }
