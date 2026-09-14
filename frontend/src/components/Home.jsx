42453aa5c81da3c5c060348cbd947e4cf183fad7
GautamKr2 <gautamkumar11008@gmail.com>
2026-09-14 11:12:54 +0530

    Members section UI completed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ import Members from "./Members";
 export default function Home() {
     return (
         <>
-            <div className="h-[60vh] bg-[url('/kali_img.jpeg')] bg-contain bg-center mt-4 md:mt-8"></div>
+            <div className="h-[60vh] bg-[url('/kali_img.jpeg')] bg-contain bg-center mt-4 md:mt-8 mx-2 md:mx-20"></div>
 
             <h2 className="text-2xl md:text-4xl text-[#f9720bef] font-bold text-center my-2"> Welcome </h2>
 
--- frontend/src/components/Members.jsx
+++ frontend/src/components/Members.jsx
@@ -2,34 +2,84 @@ export default function() {
     const members = [
         {
             id: 1,
-            name: "Gautam",
+            name: "Gautam Kumar",
             image: "/kali_img.jpeg",
             role: "President"
         },
         {
             id: 2,
-            name: "Gautam",
+            name: "Gautam Kumar",
             image: "/kali_img.jpeg",
             role: "President"
         },
         {
             id: 3,
-            name: "Gautam",
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
+            image: "/kali_img.jpeg",
+            role: "President"
+        },
+        {
+            id: 2,
+            name: "Gautam Kumar",
             image: "/kali_img.jpeg",
             role: "President"
         }
     ]
     return (
         <>
-            <section>
-                <h1> Executive Members </h1>
-                <div className="grid grid-cols-3 gap-6">
+            <section className="bg-[#f7b398] m-2 md:m-6 md:mx-[12%] rounded-md">
+                <h1 className="text-center text-xl md:text-4xl text-[#f9720bef] font-bold mt-4 md:m-4 pt-2 md:pt-4"> Executive Members </h1>
+                <div className="m-2 md:mx-4 grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-4 pb-3 md:pb-4">
                     {
                         members.map((member) => (
-                            <div key={member.id}>
-                                <img src={member.image} alt={`${member.name} profile`} className="size-40" />
-                                <h3> {member.name} </h3>
-                                <p> {member.role} </p>
+                            <div key={member.id} className=" bg-[#f3c6b4] rounded-lg shadow-md">
+                                <img src={member.image} alt={`${member.name} profile`} className="mx-auto size-26 md:size-48 rounded-md mt-2 md:mt-4 object-cover" />
+                                <div className="text-center m-2">
+                                    <h3 className="text-[12px] md:text-[16px] text-[#080808]"> {member.name} </h3>
+                                    <p className="text-[11px] md:text-[13px]  text-red-700"> {member.role} </p>
+                                </div>
                             </div>
                         ))
                     }
