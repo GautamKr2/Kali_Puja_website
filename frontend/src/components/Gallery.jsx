65b666071e9b7e60fb9ca4dc32dfec167ca5377c
GautamKr2 <gautamkumar11008@gmail.com>
2026-09-18 14:35:53 +0530

    Updated gallery section

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function Gallery() {
     const gallery = [
         {
@@ -68,19 +70,35 @@ export default function Gallery() {
         }
     ]
 
+    const [showAll, setShowAll] = useState(false);
+    const visibleImages = showAll
+        ? gallery
+        : gallery.slice(0, 8)
+
     return (
         <>
             <section className="bg-[#f7b398] m-2 md:m-6 md:mx-[12%] rounded-md">
                 <h1 className="text-center pt-3 md:pt-6 text-xl md:text-4xl text-[#f9720bef] font-bold"> Gallery/Videos </h1>
                 <div className="m-2 md:m-6 grid grid-cols-2 md:grid-cols-4 gap-4">
                     {
-                        gallery.map((image) => (
+                        visibleImages.map((image) => (
                             <div key={image.id} className="bg-[#f3c6b4] rounded-md">
                                 <img src={image.image} className="size-36 md:size-50 mx-auto my-3 md:my-4 object-cover" />
                             </div>
                         ))
                     }
                 </div>
+
+                {
+                    gallery.length > 8 && (
+                        <div className="text-center text-xl mx-auto pb-3">
+                            <button onClick={() => setShowAll(!showAll)}
+                                className="bg-[#f3c6b4] text-[#f9720bef] font-semibold px-7 py-1 cursor-pointer rounded-lg hover:bg-[#fa9e79]">
+                                {showAll ? "Show less images" : "Show more images"}
+                            </button>
+                        </div>
+                    )
+                }
             </section>
         </>
     )
